25f1c197ef34af82ea506660b4ad955dd51c039b
hRebb <119111035+hRebb@users.noreply.github.com>
2023-02-22 11:30:04 +0000

    definition of the project structure

--- index.ts
+++ index.ts
@@ -3,6 +3,22 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+interface Task {
+    title: string;
+    done: boolean;
+}
+
+const tasks: Task[] = [
+    {
+        title: "Apprendre à programmer",
+        done: false,
+    },
+    {
+        title: "Faire les courses",
+        done: true,
+    }
+];
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req: Request, res: Response) => {
